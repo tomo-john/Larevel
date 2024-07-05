801b604efe8720e3861a58a7d275844cea3e3139
tomo-john <keririnkery.kery@gmail.com>
2024-07-05 11:09:39 +0900

    add: task

--- src/Laravel9TaskList/database/seeders/TasksTableSeeder.php
+++ src/Laravel9TaskList/database/seeders/TasksTableSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
 class TasksTableSeeder extends Seeder
 {
     /**
