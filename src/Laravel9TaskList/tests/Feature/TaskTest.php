4aa06e49b29af2609bb9ce4eca083988d5a8fc24
tomo-john <keririnkery.kery@gmail.com>
2024-07-11 10:31:30 +0900

    add: test

--- src/Laravel9TaskList/tests/Feature/TaskTest.php
+++ src/Laravel9TaskList/tests/Feature/TaskTest.php
@@ -64,4 +64,26 @@ class TaskTest extends TestCase
             'due_date' => '期限日 には今日以降の日付を入力してください。',
         ]);
     }
+
+		/**
+		* 状態（セレクトボックスの値）が定義された値ではない場合はバリデーションエラーにするメソッド
+		* 用途：状態（'status'）に不正な値（1,2,3以外）を入力してエラーをテストする
+		* 
+		* @test
+		*/
+		public function status_should_be_within_defined_numbers()
+		{
+				$this->seed('TasksTableSeeder');
+
+				$response = $this->post('/folders/1/tasks/1/edit', [
+						'title' => 'Sample task',
+						'due_date' => Carbon::today()->format('Y/m/d'),
+						'status' => 999,
+				]);
+
+				$response->assertSessionHasErrors([
+						'status' => '状態 には 未着手、着手中、完了 のいずれかを指定してください。',
+				]);
+		}
+
 }
