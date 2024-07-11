e0019ebb1760e7af59b00c1305999b9cbaed4905
tomo-john <keririnkery.kery@gmail.com>
2024-07-11 20:28:58 +0900

    add: routes

--- src/Laravel9TaskList/routes/web.php
+++ src/Laravel9TaskList/routes/web.php
@@ -19,41 +19,45 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-/* home page */
-Route::get('/', [HomeController::class,"index"])->name('home');
+/*
+ * 認証を求めるミドルウェアのルーティング
+ * 機能：ルートグループによる一括適用とミドルウェアによるページ認証
+ * 用途：全てのページに対してページ認証を求める
+ */
 
-/* homepage commentout
-Route::get('/', function () {
-    return view('welcome');
+	Route::group(['middleware' => 'auth'], function() {
+
+	/* home page */
+	Route::get('/', [HomeController::class,"index"])->name('home');
+
+	/* index page */
+	Route::get("/folders/{id}/tasks", [TaskController::class,"index"])->name("tasks.index");
+
+	/* folders new create page */
+	Route::get('/folders/create', [FolderController::class,"showCreateForm"])->name('folders.create');
+	Route::post('/folders/create', [FolderController::class,"create"]);
+
+	/* tasks new create page */
+	Route::get('/folders/{id}/tasks/create', [TaskController::class,"showCreateForm"])->name('tasks.create');
+	Route::post('/folders/{id}/tasks/create', [TaskController::class,"create"]);
+
+	/* folders new edit page */
+	Route::get('/folders/{id}/edit', [FolderController::class,"showEditForm"])->name('folders.edit');
+	Route::post('/folders/{id}/edit', [FolderController::class,"edit"]);
+
+	/* tasks new edit page */
+	Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class,"showEditForm"])->name('tasks.edit');
+	Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class,"edit"]);
+
+	/* folders new delete page */
+	Route::get('/folders/{id}/delete', [FolderController::class,"showDeleteForm"])->name('folders.delete');
+	Route::post('/folders/{id}/delete', [FolderController::class,"delete"]);
+
+	/* tasks new delete page */
+	Route::get('/folders/{id}/tasks/{task_id}/delete', [TaskController::class,"showDeleteForm"])->name('tasks.delete');
+	Route::post('/folders/{id}/tasks/{task_id}/delete', [TaskController::class,"delete"]);
+
 });
-*/
-
-/* index page */
-Route::get("/folders/{id}/tasks", [TaskController::class,"index"])->name("tasks.index");
-
-/* folders new create page */
-Route::get('/folders/create', [FolderController::class,"showCreateForm"])->name('folders.create');
-Route::post('/folders/create', [FolderController::class,"create"]);
-
-/* tasks new create page */
-Route::get('/folders/{id}/tasks/create', [TaskController::class,"showCreateForm"])->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', [TaskController::class,"create"]);
-
-/* folders new edit page */
-Route::get('/folders/{id}/edit', [FolderController::class,"showEditForm"])->name('folders.edit');
-Route::post('/folders/{id}/edit', [FolderController::class,"edit"]);
-
-/* tasks new edit page */
-Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class,"showEditForm"])->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class,"edit"]);
-
-/* folders new delete page */
-Route::get('/folders/{id}/delete', [FolderController::class,"showDeleteForm"])->name('folders.delete');
-Route::post('/folders/{id}/delete', [FolderController::class,"delete"]);
-
-/* tasks new delete page */
-Route::get('/folders/{id}/tasks/{task_id}/delete', [TaskController::class,"showDeleteForm"])->name('tasks.delete');
-Route::post('/folders/{id}/tasks/{task_id}/delete', [TaskController::class,"delete"]);
 
 /* certification page （会員登録・ログイン・ログアウト・パスワード再設定など） */
 Auth::routes();
