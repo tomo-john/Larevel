f2b2a3ea5b3cadbfb44129f688e1d7e26a9d9cfa
tomo-john <keririnkery.kery@gmail.com>
2024-07-11 11:16:36 +0900

    add: seed

--- src/Laravel9TaskList/database/seeders/DatabaseSeeder.php
+++ src/Laravel9TaskList/database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(UsersTableSeeder::class);
+        $this->call(FoldersTableSeeder::class);
         $this->call(TasksTableSeeder::class);
     }
 }
