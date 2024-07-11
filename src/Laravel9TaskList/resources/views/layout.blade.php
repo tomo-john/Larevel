7c37de4e5f55542aba8185ea3bf6ba3ba472bbbd
tomo-john <keririnkery.kery@gmail.com>
2024-07-11 20:17:46 +0900

    add: login

--- src/Laravel9TaskList/resources/views/layout.blade.php
+++ src/Laravel9TaskList/resources/views/layout.blade.php
@@ -7,16 +7,43 @@
     <title>ToDo App</title>
     @yield('styles')
     <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
+	</head>
+	<body>
     <header>
-        <nav class="my-navbar">
-            <a class="my-navbar-brand" href="/">ToDo App</a>
-        </nav>
-    </header>
+			<nav class="my-navbar">
+					<a class="my-navbar-brand" href="/">ToDo App</a>
+					<div class="my-navbar-control">
+							@if(Auth::check())
+									<span class="my-navbar-item">ようこそ, {{ Auth::user()->name }}さん</span>
+									｜
+									<a href="#" id="logout" class="my-navbar-item">ログアウト</a>
+									<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+											@csrf
+									</form>
+							@else
+									<a class="my-navbar-item" href="{{ route('login') }}">ログイン</a>
+									｜
+									<a class="my-navbar-item" href="{{ route('register') }}">会員登録</a>
+							@endif
+					</div>
+			</nav>
+		</header>
     <main>
         @yield('content')
     </main>
+			<!--
+			*   ログアウトのクリックイベント
+			*   機能：ログアウトリンクのクリック時に真下のログアウトフォームを送信する
+			*   用途：ログアウトを実施する
+			-->
+			@if(Auth::check())
+					<script>
+							document.getElementById('logout').addEventListener('click', function(event) {
+							event.preventDefault();
+							document.getElementById('logout-form').submit();
+							});
+					</script>
+			@endif
     @yield('scripts')
 </body>
 </html>
