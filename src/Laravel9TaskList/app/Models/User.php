485b7a30ff26f59d7b643cff886cda58ac362019
tomo-john <keririnkery.kery@gmail.com>
2024-07-11 11:11:46 +0900

    add: user

--- src/Laravel9TaskList/app/Models/User.php
+++ src/Laravel9TaskList/app/Models/User.php
@@ -12,6 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    // テーブル名を明示的に指定する
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
@@ -41,4 +44,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /*
+    * ユーザークラスの関係性を辿ってフォルダークラスのリストを取得する
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    */
+    public function folders()
+    {
+        return $this->hasMany('App\Models\Folder');
+    }
+
 }
